a0d0fa220f37c5bf1433045509ea21847d4d2c94
Armando0531 <armandoenriquez0531@gmail.com>
2024-02-28 18:28:49 -0600

    Se configuro firebase en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"angular-proyecto-e6c9e","appId":"1:814960605357:web:0996acf58df28f8cd76e24","storageBucket":"angular-proyecto-e6c9e.appspot.com","apiKey":"AIzaSyC1SvPgVfERPkj7kzDYaG9nXQ95ZoruidE","authDomain":"angular-proyecto-e6c9e.firebaseapp.com","messagingSenderId":"814960605357"})),
+    provideAuth(() => getAuth())
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
